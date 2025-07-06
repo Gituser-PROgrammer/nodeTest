4728d16c8ed858e6c46b8a47fb3a55750f74e156
Gituser-PROgrammer <139343533+Gituser-PROgrammer@users.noreply.github.com>
2025-07-06 13:03:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200; // OK
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world! This is my first Node.js server.\n');
+  res.end('Hello, world! This is my first Node.js server This is Gituser-PROgrammer....\n');
 });
 
 const PORT = 3000;
